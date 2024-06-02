5d7b3630733cb86447cd39ef4ce0f7e272c2ff1f
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-02 11:36:55 -0500

    interface

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,63 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  List<Map<String, dynamic>> questionsList = [
+    {"question": "¿El hombre llegó a la luna?", "asnwer": true},
+    {"question": "¿Has almorzado hoy?", "asnwer": false},
+    {"question": "¿Sientes frio?", "asnwer": false},
+    {"question": "¿Saliste ayer?", "asnwer": false},
+    {"question": "¿Has desayunado?", "asnwer": true},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff2B2E42),
+      appBar: AppBar(
+        title: Text("Quizz App"),
+        centerTitle: true,
+        backgroundColor: Color(0xff2B2E42),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: Text(
+                  "Esta es la pregunta",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  onPressed: () {},
+                  color: Colors.greenAccent,
+                  child: Text("Verdadero"),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  onPressed: () {},
+                  color: Colors.redAccent,
+                  child: Text("false"),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
